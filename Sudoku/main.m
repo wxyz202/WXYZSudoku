060250aaa34e142d0efcf88eab0fa70c88e86c6d
WXYZ <zsu_wxyz@163.com>
2014-01-04 11:25:04 +0800

    Initial commit

---
+++ Sudoku/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sudoku
+//
+//  Created by wxyz on 14-1-4.
+//  Copyright (c) 2014年 wxyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WXYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WXYZAppDelegate class]));
+    }
+}
